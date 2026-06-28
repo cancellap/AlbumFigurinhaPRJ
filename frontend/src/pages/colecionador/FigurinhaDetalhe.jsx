89325888791d56f2eb74e8af300cfe69bfb77bdf
Natan Mauricio Santos <natanmauriciosantos@hotmail.com>
2026-06-28 17:39:54 -0300

    Atualizando FigurinhaDetalhe.jsx

--- frontend/src/pages/colecionador/FigurinhaDetalhe.jsx
+++ frontend/src/pages/colecionador/FigurinhaDetalhe.jsx
@@ -1,6 +1,67 @@
 import "../../styles/album.css";
 
+import { useEffect, useState } from "react";
+import { useLocation } from "react-router-dom";
+
+import {
+    buscarFigurinha,
+    fotoFigurinha
+} from "../../services/sticker.service";
+
 export default function FigurinhaDetalhe() {
+
+    const location = useLocation();
+
+    const id = location.state?.id;
+
+    const [figurinha, setFigurinha] = useState(null);
+    const [erro, setErro] = useState("");
+
+    useEffect(() => {
+
+        if (!id) {
+            return;
+        }
+
+        carregarFigurinha();
+
+    }, []);
+
+    async function carregarFigurinha() {
+
+        try {
+
+            const dados = await buscarFigurinha(id);
+
+            setFigurinha(dados);
+
+        } catch {
+
+            setErro("Erro ao carregar figurinha.");
+
+        }
+
+    }
+
+    if (erro) {
+
+        return (
+            <div className="page">
+                <h2>{erro}</h2>
+            </div>
+        );
+
+    }
+
+    if (!figurinha) {
+
+        return (
+            <div className="page">
+                <h2>Carregando...</h2>
+            </div>
+        );
+
+    }
 
     return (
 
@@ -11,16 +72,44 @@ export default function FigurinhaDetalhe() {
             <div className="card-detalhe">
 
                 <img
-                    src="https://placehold.co/250x350"
-                    alt="Figurinha"
+                    src={fotoFigurinha(figurinha.id)}
+                    alt={figurinha.nome}
                 />
 
                 <h2>
-                    Figurinha 001
+
+                    #{figurinha.numero} - {figurinha.nome}
+
                 </h2>
 
                 <p>
-                    Categoria: Futebol
+
+                    <strong>Página:</strong> {figurinha.pagina}
+
+                </p>
+
+                <p>
+
+                    <strong>Descrição:</strong>
+
+                </p>
+
+                <p>
+
+                    {figurinha.descricao || "Sem descrição."}
+
+                </p>
+
+                <p>
+
+                    <strong>Tag MD5:</strong>
+
+                </p>
+
+                <p>
+
+                    {figurinha.tag}
+
                 </p>
 
             </div>
@@ -28,4 +117,5 @@ export default function FigurinhaDetalhe() {
         </div>
 
     );
+
 }
